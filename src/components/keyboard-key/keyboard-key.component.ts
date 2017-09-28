c6fcb160532ed8d36e4e8dc105880435396ccfba
Andres Fabian Diaz <andresdkm@gmail.com>
2017-09-28 11:46:55 -0500

    Add NgModel

--- src/components/keyboard-key/keyboard-key.component.ts
+++ src/components/keyboard-key/keyboard-key.component.ts
@@ -224,7 +224,9 @@ export class MdKeyboardKeyComponent implements OnInit {
       return;
     }
 
-    this.inputValue = this.control.value;
+    if (this.control) {
+      this.inputValue = this.control.value;
+    }
     // ^ this is used to not only get "focus", but
     // to make sure we don't have it everything -selected-
     // (it causes an issue in chrome, and having it doesn't hurt any other browser)
